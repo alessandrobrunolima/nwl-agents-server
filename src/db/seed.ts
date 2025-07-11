a5b83c39a6bb4bbbbcaf21d098b9353551d43223
Alessandro Bruno Lima <alessandrobrunolima@gmail.com>
2025-07-10 21:57:05 -0300

    feat: ativar o seeding de dados para salas e perguntas com dados fictícios

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -6,21 +6,21 @@ await reset(db, schema );
 
 const seedSchema = { rooms: schema.rooms, questions: schema.questions };
 
-// await seed(db, seedSchema ).refine((f) => {
-//     return {
-//         rooms: {
-//             count: 5,
-//             columns: {
-//                 name: f.companyName(),
-//                 description: f.loremIpsum(),
-//                 createdAt: f.date({ minDate: new Date(2025, 0, 1), maxDate: new Date(2025, 6, 1) })
-//             }
-//         },
-//         questions: {
-//             count: 20
-//         }
-//     }
-// });
+await seed(db, seedSchema ).refine((f) => {
+    return {
+        rooms: {
+            count: 5,
+            columns: {
+                name: f.companyName(),
+                description: f.loremIpsum(),
+                createdAt: f.date({ minDate: new Date(2025, 0, 1), maxDate: new Date(2025, 6, 1) })
+            }
+        },
+        questions: {
+            count: 20
+        }
+    }
+});
 
 await sql.end();
 
